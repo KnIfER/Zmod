504d5f92f2280cd094d442cd0cf65aa4ad55b821
KnIfER <knziha@gmail.com>
2019-11-20 16:04:52 +0800

    '1'

---
+++ models/models.js
@@ -0,0 +1,97 @@
+o3v.MODELS = [
+        {
+            id: 'adult_female:131011',
+            name: 'adult_female.obj',
+            label: 'Adult Female',
+            scriptFile: 'models/adult_female2/adult_female.js',
+            metadataFile: 'models/adult_female2/entity_metadata.json',
+            layerIconsFile: 'models/adult_female2/layer_icons.png',
+            modelUri: '',
+            textureUri: '',
+            numLayers: 8
+        },
+        {
+            id: 'adult_male:130601',
+            name: 'adult_male.obj',
+            label: 'Adult Male',
+            scriptFile: 'models/adult_male2/adult_male.js',
+            metadataFile: 'models/adult_male2/entity_metadata.json',
+            layerIconsFile: 'models/adult_male2/layer_icons.png',
+            modelUri: '',
+            textureUri: '',
+            numLayers: 8
+        },
+        {
+            id: 'Lateral_Heart:20140228',
+            name: 'Heart_Lateral_Cut.obj',
+            label: 'Heart Lateral Cut',
+            scriptFile: 'models/Heart_Lateral_Cut/Heart_Lateral_Cut.js',
+            metadataFile: 'models/Heart_Lateral_Cut/entity_metadata.json',
+            layerIconsFile: 'models/Heart_Lateral_Cut/layer_icons.png',
+            modelUri: '',
+            textureUri: '',
+            uiPath: 'Components',
+            numLayers: 1
+        },
+        {
+            id: 'Anterior_Cut_Heart:20140228',
+            name: 'Anterior_Cut_Heart.obj',
+            label: 'Heart Anterior Cut',
+            scriptFile: 'models/Heart_Anterior_Cut/Anterior_Cut_Heart.js',
+            metadataFile: 'models/Heart_Anterior_Cut/entity_metadata.json',
+            layerIconsFile: 'models/Heart_Anterior_Cut/layer_icons.png',
+            modelUri: '',
+            textureUri: '',
+            uiPath: 'Components',
+            numLayers: 1
+        },
+        {
+            id: 'Heart top cut:20140228',
+            name: 'Heart_Top_Cut.obj',
+            label: 'Heart Top Cut',
+            scriptFile: 'models/Heart_Top_Cut/Heart_Top_Cut.js',
+            metadataFile: 'models/Heart_Top_Cut/entity_metadata.json',
+            layerIconsFile: 'models/Heart_Top_Cut/layer_icons.png',
+            modelUri: '',
+            textureUri: '',
+            uiPath: 'Components',
+            numLayers: 1
+        },
+        {
+            id: 'Eye:20140228',
+            name: 'Eye.obj',
+            label: 'Eye',
+            scriptFile: 'models/Eye/Eye.js',
+            metadataFile: 'models/Eye/entity_metadata.json',
+            layerIconsFile: 'models/Eye/layer_icons.png',
+            modelUri: '',
+            textureUri: '',
+            uiPath: 'Components',
+            numLayers: 1
+        },
+        {
+            id: 'skull:20140228',
+            name: 'Articulating_Skull.obj',
+            label: 'Articulating Skull',
+            scriptFile: 'models/Articulating_Skull/Articulating_Skull.js',
+            metadataFile: 'models/Articulating_Skull/entity_metadata.json',
+            layerIconsFile: 'models/Articulating_Skull/layer_icons.png',
+            modelUri: '',
+            textureUri: '',
+            uiPath: 'Components',
+            numLayers: 2
+        },
+        {
+            id: 'Ear:20140228',
+            name: 'Ear_Cross_Section.obj',
+            label: '*NEW* Ear Cross Section',
+            baseUrl: '',
+            scriptFile: 'models/Ear_Cross_Section/Ear_Cross_Section.js',
+            metadataFile: 'models/Ear_Cross_Section/entity_metadata.json',
+            layerIconsFile: 'models/Ear_Cross_Section/layer_icons.png',
+            modelUri: '',
+            textureUri: '',
+            uiPath: 'Components',
+            numLayers: 1
+        }
+    ];
